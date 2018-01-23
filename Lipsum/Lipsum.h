aa8b6efc7d961d3fd5dc3daf9ad98912f7b46a6a
Xavier Rubio Jansana <xavier@toro-intl.com>
2018-01-23 22:04:29 +0100

    Initial Commit

---
+++ Lipsum/Lipsum.h
@@ -0,0 +1,19 @@
+//
+//  Lipsum.h
+//  Lipsum
+//
+//  Created by Xavier Rubio Jansana on 23/1/18.
+//  Copyright © 2018 xrubio.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lipsum.
+FOUNDATION_EXPORT double LipsumVersionNumber;
+
+//! Project version string for Lipsum.
+FOUNDATION_EXPORT const unsigned char LipsumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lipsum/PublicHeader.h>
+
+
